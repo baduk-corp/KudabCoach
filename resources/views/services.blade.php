fe52e03f4f9bb4f0e44c944a5b1935e1c5742aa7
Frederik Lima Hagemeier <frederikhagemeier@gmail.com>
2019-06-07 22:09:24 -0300

    Adicionado produtos e Servicos

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,23 +1,47 @@
 @extends('layouts.appBulma')
 
 @section('content')
-<section>
-    <div class="container">
-        <div class="columns is-marginless is-centered">
-            <div class="column is-7">
-                <nav class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                            Serviços
-                        </p>
-                    </header>
-
-                    <div class="card-content">
-                        Vá a merda!
+<section class="container">
+        <div class="columns features">
+            <div class="column is-4">
+                <div class="card is-shady">
+                    <div class="card-image has-text-centered">
+                        <i class="fas fa-home"></i>
                     </div>
-                </nav>
+                    <div class="card-content">
+                        <div class="content">
+                            <h4 class="is-size-4">Consultas presenciais</h4>
+                            <p>Consultas motivacionais direto na sua casa ou em nosso consultorio!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="card is-shady">
+                    <div class="card-image has-text-centered">
+                        <i class="fas fa-microphone-alt"></i>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <h4 class="is-size-4">Palestras</h4>
+                            <p>Contrate minhas palestras para motivacionar sua equipe!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-4">
+                <div class="card is-shady">
+                    <div class="card-image has-text-centered">
+                        <i class="fas fa-desktop"></i>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <h4 class="is-size-4">Consultas Online</h4>
+                            <p>Consultas motivacionais online a hora que voce precisar!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
