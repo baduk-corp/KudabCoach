6e584023e329e0f2969877ea79edbeaf0c9c69d6
Andre Luiz Pastore Dambros <andreluizpd@gmail.com>
2019-06-19 20:02:05 -0300

    Alterado uso do forelse

--- resources/views/vip/home.blade.php
+++ resources/views/vip/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="columns is-marginless is-centered">
             <div class="column is-7">
-                @foreach($posts as $post)
+                @forelse($posts as $post)
                     <nav class="card">
                         <header class="card-header">
                             <p class="card-header-title">{{ $post->titulo }}</p>
@@ -19,7 +19,15 @@
                             <a href="{{ route('vip.read', ['id' => $post->id]) }}" class="card-footer-item">Ver mais</a>
                         </footer>
                     </nav>
-                @endforeach
+                @empty
+                    <nav class="card">
+                        <header class="card-header">
+                            <p class="card-header-title">Nenhum post ainda</p>
+                        </header>
+                        <div class="card-content">
+                        </div>
+                    </nav>
+                @endforelse
 
                 <nav class="pagination" role="navigation" aria-label="pagination">
                     {{ $posts->links('layouts.bulmaPagination') }}
